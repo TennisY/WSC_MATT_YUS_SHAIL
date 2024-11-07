15426ad9fc33009890e137fca3f7bd5144c12142
TennisY <69335505+TennisY@users.noreply.github.com>
2024-11-07 19:23:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Birthday Celebration Button
+function celebrateBirthday() {
+    document.body.style.background = "url('confetti.gif') no-repeat center center fixed";
+    alert("Happy Birthday Waterstone College! 🎉");
+}
+
+// Countdown Timer for Sports Day
+function countdownToEvent() {
+    const eventDate = new Date("April 10, 2024 00:00
